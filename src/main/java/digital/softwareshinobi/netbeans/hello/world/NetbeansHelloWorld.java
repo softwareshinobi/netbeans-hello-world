777ee62f0820ce15dcfa99d8453c1f22f12dcf64
Software Shinobi <the.software.shinobi@gmail.com>
2024-04-11 22:37:21 -0500

    initial commit with working files from video session

---
+++ src/main/java/digital/softwareshinobi/netbeans/hello/world/NetbeansHelloWorld.java
@@ -0,0 +1,28 @@
+package digital.softwareshinobi.netbeans.hello.world;
+
+/**
+ *
+ * this class is a simple test that your netbeans ide is set up correctly.
+ *
+ * if you hit run and then you see "hello world", then you wavy.
+ *
+ * now go and kick ass!
+ *
+ * @author softwareshinobi
+ */
+public class NetbeansHelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("launch > NetbeansHelloWorld");
+        System.out.println();
+
+        System.out.println("Hello Universe!");
+        System.out.println();
+
+        System.out.println("If you're reading this it's too late & netbeans is ready to put in work.");
+        System.out.println();
+
+    }
+
+}
